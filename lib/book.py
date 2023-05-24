839e18797fc9719048ed251b8591ea0fefa1cb3f
Scholastica Nzomo <scholambula1@gmail.com>
2023-05-24 12:47:02 +0300

    Done with Assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def get_pages(self):
+        return self._page_count
+
+    def set_pages(self, pages):
+        if type(pages) == int:
+            self._page_count = pages
+        else:
+            print("page_count must be an integer" )
+
+    page_count = property(get_pages, set_pages)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
+
     
         
